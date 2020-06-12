f38a0f8211bf83cb8d9def22084c9c1a2b0731d0
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-06-12 11:59:01 -0500

    first 10 problems for JS1

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -2,63 +2,84 @@
 
 // Create a variable called 'myName' with a value that is your name. 
 
-// Code here
+const myName = "Veronica";
 
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' with a value that is your favorite number. 
 
-//Code here
+const faveNum = 7;
 
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and assign it a value of true.
 
-//Code here
+const lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 
-// Create a function called 'sum' that takes in two parameters, 'num1' and 'num2'. The function should return the sum of the two parameters.
+// Create a function called 'sum' that takes in two parameters, 'num1' and 'num2'.
+// The function should return the sum of the two parameters.
 
-//Code here
+function sum(num1, num2) {
+	return num1 + num2;
+}
 
 //////////////////PROBLEM 5////////////////////
 
-// Create a function called 'lovesCodeChecker' that takes in a single parameter called 'x'. Check to see if 'x' is equal to true. If it is, return a true boolean. If 'x' does not equal true, return a false boolean.  
+// Create a function called 'lovesCodeChecker' that takes in a single parameter called 'x'.
+// Check to see if 'x' is equal to true. If it is, return a true boolean. If 'x' does not equal true, return a false boolean.
 
-//Code here
+function lovesCodeChecker(x) {
+	if (x === true) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 //////////////////PROBLEM 6////////////////////
 
-// Create a function called 'oddChecker' that takes in one parameter, 'num'. Your function should check to see if the parameter is even or odd. If the parameter is odd, return 'the number is odd'. If the number is even, return 'the number is even'.
+// Create a function called 'oddChecker' that takes in one parameter, 'num'.
+// Your function should check to see if the parameter is even or odd. If the parameter is odd, return 'the number is odd'.
+// If the number is even, return 'the number is even'.
 
-//Code here
+function oddChecker(num) {
+	if (num % 2 === 0) {
+		return "The number is even";
+	} else {
+		return "The number is odd";
+	}
+}
 
 //////////////////PROBLEM 7////////////////////
 
-// Create a function called 'iLove' that takes in two string parameters, 'name' and 'love'. Have the function take the two parameters and return a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Joseph loves music"
+// Create a function called 'iLove' that takes in two string parameters, 'name' and 'love'.
+// Have the function take the two parameters and return a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Joseph loves music"
 
-//Code Here
+function iLove(name, love) {
+	return `${name} loves ${love}`;
+}
 
 //////////////////PROBLEM 8////////////////////
 
 // Create a copy of the faveColors array called 'colorCopy' using slice.
 const faveColors = ['red', 'green', 'black']
 
-//Code Here
+const favColCopy = faveColors.slice();
 
 //////////////////PROBLEM 9////////////////////
 
 // Add a fourth color to the end of the 'colorCopy' array using push.
 
-//Code Here
+favColCopy.push("purple");
 
 //////////////////PROBLEM 10////////////////////
 
 // Create a new array called 'middleNums' from the numbers array that will capture only the middle numbers (2, 3, 4).
 const numbers = [1, 2, 3, 4, 5]
 
-//Code Here
+const middleNums = numbers.slice(1, 3);
 
 //////////////////PROBLEM 11////////////////////
 
